55857362c43446a8655e623d355edd6fafbac021
kasra-l83 <o.kasralatifi@gmail.com>
2024-11-13 17:54:24 +0330

    Logo part

--- main.js
+++ main.js
@@ -1,14 +1,21 @@
 const modal= document.getElementById("modal");
-
 function open(){
     modal.style.display= "block"
 }
 function close(){
     modal.style.display= "none"
 }
-
 document.getElementById("menu").onclick= function(){
     if(modal.style.display=== "none"){
         open()
     }else close()
+}
+
+document.getElementById("close").onclick= function(){
+    modal1.style.display= "none"
+}
+const modal1= document.getElementById("black");
+document.getElementById("open").onclick= function(){
+    modal1.style.display= "flex";
+    modal1.style.justifyContent= "center";
 }
